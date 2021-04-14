696b11e1256515442f7bf0441f8be3e4c9a9a63a
gredman2018 <43188682+gredman2018@users.noreply.github.com>
2021-04-14 15:47:37 -0400

    added mobile responsiveness

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # spell_database
+
+this is an nyu ima project designed to 
+
+1. combat cultural stigmas against alternative spiritual & religious practices
+
+2. make spellwork accessible
+
+3. decolonize non-standard spiritual/religious/occcult/magical practices
+
+4. encourage self growth, healing and love
  
---
+++ code.js
@@ -0,0 +1,24 @@
+
+
+// var menu_button = document.getElementById('menu');
+// var navbar = document.getElementById('navbar');
+// var click = false;
+
+// menu_button.onclick = function showButton(){
+// 	if (click == false){
+
+// 	navbar.classList.remove('hidden');
+// 	navbar.classList.add('show');
+// 	console.log('lcik!');	
+
+// 	}else{
+
+// 	click = true;
+// 	navbar.classList.remove('show');
+// 	navbar.classList.add('hidden');
+
+// }
+
+// }
+
+
